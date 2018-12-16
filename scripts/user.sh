245f6243e5aa23536cde129383246561ea20a05e
Jose Luis Duran <jlduran@users.noreply.github.com>
2018-12-16 14:52:16 -0200

    Use here documents

--- scripts/user.sh
+++ scripts/user.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -e
 
 # Initialize rbenv
-# shellcheck disable=SC2016
-printf 'export RBENV_NATIVE_EXT=1\neval "$(rbenv init -)"\n' > \
-	/home/vagrant/.bash_profile
+cat > /home/vagrant/.bash_profile << END
+export RBENV_NATIVE_EXT=1
+eval "$(rbenv init -)"
+END
 
 # Install Ruby as the vagrant user
 RUBY_CONFIGURE_OPTS=--disable-install-doc rbenv install "$1"
